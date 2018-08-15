e286c70f7e7131dba705f0200cf8057bb179c06e
John Zhang <johntostring@gmail.com>
2018-08-15 14:20:06 +0800

    update spring-boot to 2.0.4.RELEASE. shiro to 1.4.0

--- spring-boot-shiro-autoconfigure/src/main/java/com/millinch/spring/boot/autoconfigure/shiro/ShiroAutoConfiguration.java
+++ spring-boot-shiro-autoconfigure/src/main/java/com/millinch/spring/boot/autoconfigure/shiro/ShiroAutoConfiguration.java
@@ -31,7 +31,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
